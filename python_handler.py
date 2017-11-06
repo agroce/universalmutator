de6626333ed573295ce08531441f6a3f6ccaf88e
Alex Groce <agroce@gmail.com>
2017-11-06 12:29:20 -0800

    Major reorganization, tools handles all languages now

---
+++ python_handler.py
@@ -0,0 +1,34 @@
+import marshal
+import os
+import subprocess
+
+def getPythonCode(fname):
+    # Courtesy of Ned Batchelder, just get the code object from the .pyc file
+    f = open(fname, "rb")
+    magic = f.read(4)
+    moddate = f.read(4)
+    # Note that for Python 3.3+ you'd need another f.read(4) here since the format changed
+    code = marshal.load(f)
+    return code
+
+def handler(tmpMutantName, mutant, sourceFile, uniqueMutants):
+    if len(uniqueMutants) == 0:
+            sourceCompiled = sourceFile.replace(".py",".pyc")
+            if os.path.exists(sourceCompiled):
+                uniqueMutants [getPythonCode(sourceCompiled)] = 1
+    
+    compiled = tmpMutantName.replace(".py",".pyc")
+    if os.path.exists(compiled):
+        os.remove(compiled)
+    with open("mutant_output",'w') as file:
+        subprocess.call(["python","handlemutant.py"],stdout=file,stderr=file)
+    if os.path.exists(compiled):
+        code = getPythonCode(compiled)
+        if code in uniqueMutants:
+            uniqueMutants[code] += 1
+            return "REDUNDANT"
+        else:
+            uniqueMutants[code] = 1
+            return "VALID"
+    else:
+        return "INVALID"
