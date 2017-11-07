5063d89bde84dad1c18f2a65ed4aac415e52ddec
Alex Groce <agroce@gmail.com>
2017-11-07 12:42:02 -0800

    swift now checks for redundant mutants

--- swift_handler.py
+++ swift_handler.py
@@ -1,10 +1,24 @@
 import subprocess
+import shutil
 
 def handler(tmpMutantName, mutant, sourceFile, uniqueMutants):
-
+    if len(uniqueMutants) == 0:
+        shutil.copy(tmpMutantName,tmpMutantName+".backup")
+        shutil.copy(sourceFile,tmpMutantName)
+        with open("mutant_output",'w') as file:    
+            r = subprocess.call(["swiftc",tmpMutantName],stdout=file,stderr=file)
+        with open(tmpMutantName.replace(".swift",""),'rb') as file:
+            uniqueMutants[file.read()] = 1
     with open("mutant_output",'w') as file:    
         r = subprocess.call(["swiftc",tmpMutantName],stdout=file,stderr=file)
     if r == 0:
-        return "VALID"
+        with open(tmpMutantName.replace(".swift",""),'rb') as file:
+            code = file.read()
+        if code in uniqueMutants:
+            uniqueMutants[code] += 1
+            return "REDUNDANT"
+        else:
+            uniqueMutants[code] = 1
+            return "VALID"
     else:
         return "INVALID"
