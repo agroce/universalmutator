b90d993c66df4e4605bb40706bc270addae6ccea
Alex Groce <agroce@gmail.com>
2017-11-07 16:08:21 -0800

    new pip version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='universalmutator',
-    version='0.3',
+    version='0.5',
     description='Universal regexp-based mutation tool',
     long_description=open('README.md').read(),
     packages=['universalmutator',],
