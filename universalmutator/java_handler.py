e31febd7416e24532565909bff71b5704a6e29c1
Alex Groce <agroce@gmail.com>
2017-11-19 09:11:55 -0800

    ok

--- universalmutator/java_handler.py
+++ universalmutator/java_handler.py
@@ -3,19 +3,7 @@ import shutil
 import os
 
 def handler(tmpMutantName, mutant, sourceFile, uniqueMutants):
-    return "VALID"
-    classFile = sourceFile.replace(".java",".class")    
-    if len(uniqueMutants) == 0:
-        try:
-            if os.path.exists(classFile):
-                shutil.copy(classFile,".um.backup."+classFile)                        
-            with open(".um.mutant_output",'w') as file:    
-                r = subprocess.call(["javac",sourceFile],stdout=file,stderr=file)
-            with open(classFile,'rb') as file:
-                uniqueMutants[file.read()] = 1
-        finally:
-            if os.path.exists(".um.backup."+classFile):
-                shutil.copy(".um.backup."+classFile,classFile)                            
+    classFile = sourceFile.replace(".java",".class")                       
     try:
         shutil.copy(sourceFile,".um.backup."+sourceFile)
         if os.path.exists(classFile):
@@ -28,13 +16,6 @@ def handler(tmpMutantName, mutant, sourceFile, uniqueMutants):
         if os.path.exists(".um.backup."+classFile):
             shutil.copy(".um.backup."+classFile,classFile)                
     if r == 0:
-        with open(classFile,'rb') as file:
-            code = file.read()
-        if code in uniqueMutants:
-            uniqueMutants[code] += 1
-            return "REDUNDANT"
-        else:
-            uniqueMutants[code] = 1
-            return "VALID"
+        return "VALID"
     else:
         return "INVALID"
