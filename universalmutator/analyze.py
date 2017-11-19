743715de84a5f06828795be2a4e27ce747200d8d
Alex Groce <agroce@gmail.com>
2017-11-18 16:01:40 -0800

    mutation score printed

--- universalmutator/analyze.py
+++ universalmutator/analyze.py
@@ -16,27 +16,34 @@ def main():
 
     srcEnd = src.split(".")[-1]
 
+    count = 0.0
+    killCount = 0.0
+
     with open("killed.txt",'w') as killed:
         with open ("notkilled.txt",'w') as notkilled:
             for f in glob.glob(src.replace(srcEnd,"mutant.*." + srcEnd)):
                 print f,
-                if f in ignored:
+                if f in ignore:
                     print "SKIPPED"
                 try:
                     shutil.copy(src,".backup."+src)
                     shutil.copy(f,src)
                     with open(os.devnull,'w') as dnull:
                         r = subprocess.call(tstCmd,shell=True,stderr=dnull,stdout=dnull)
+                    count += 1
                     if r == 0:
                         print "NOT KILLED"
                         notkilled.write(f+"\n")
                         notkilled.flush()
                     else:
+                        killCount += 1
                         print "KILLED"
                         killed.write(f+"\n")
                         killed.flush()
                 finally:
                     shutil.copy(".backup."+src,src)
+    print "MUTATION SCORE:",killCount/count
+    
 
 if __name__ == '__main__':
     main()        
