3dfb390f459555cdf5f63bd7d8302d8d35386e82
Alex Groce <agroce@gmail.com>
2018-03-10 10:42:16 -0700

    better redundancy detection for solidity assembly

--- universalmutator/solidity_handler.py
+++ universalmutator/solidity_handler.py
@@ -1,6 +1,18 @@
 import subprocess
 import shutil
 
+def extractASM(text,filename):
+    newText = ""
+    lines = text.split("\n")
+    assemblyStart = False
+    for l in lines:
+        if assemblyStart:
+            if (filename not in l) and ("auxdata: 0x" not in l):
+                newText += (l + "\n")
+        elif l.find("EVM assembly:") == 0:
+            assemblyStart = True
+    return newText
+
 def handler(tmpMutantName, mutant, sourceFile, uniqueMutants):
     if len(uniqueMutants) == 0:
         shutil.copy(tmpMutantName,tmpMutantName+".backup")
@@ -8,12 +20,12 @@ def handler(tmpMutantName, mutant, sourceFile, uniqueMutants):
         with open(".um.out.asm",'w') as file:    
             r = subprocess.call(["solc",tmpMutantName,"--asm"],stdout=file,stderr=file)
         with open(".um.out.asm",'r') as file:
-            uniqueMutants[file.read()] = 1
+            uniqueMutants[extractASM(file.read(),tmpMutantName)] = 1
     with open(".um.out.asm",'w') as file:    
         r = subprocess.call(["solc",tmpMutantName,"--asm"],stdout=file,stderr=file)
     if r == 0:
         with open(".um.out.asm",'r') as file:
-            code = file.read()
+            code = extractASM(file.read(),tmpMutantName)
         if code in uniqueMutants:
             uniqueMutants[code] += 1
             return "REDUNDANT"
