e5432a55c2e042101d2e462b55cea92dd9fa7e77
agroce <agroce@gmail.com>
2019-03-23 15:16:48 -0700

    new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='universalmutator',
-    version='0.8.29',
+    version='0.8.30',
     description='Universal regexp-based mutation tool',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
