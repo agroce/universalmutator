062ee48685186e8ea103f44bb67d949ad311f556
agroce <agroce@gmail.com>
2019-06-03 10:03:40 -0700

    new version released on pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='universalmutator',
-    version='0.9.1',
+    version='0.9.2',
     description='Universal regexp-based mutation tool',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
