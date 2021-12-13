ef7708074e20694898f4378eef7e6f2680c597a1
Kush Jain <kdjain@andrew.cmu.edu>
2021-12-13 15:25:56 -0500

    Updating casing to be consistent with UM code

--- universalmutator/analyze.py
+++ universalmutator/analyze.py
@@ -30,7 +30,7 @@ def main():
         print("       --noShuffle: do not randomize order of mutants")
         print("       --resume: use existing killed.txt and notkilled.txt, resume mutation analysis")
         print("       --prefix: add a prefix to killed.txt and notkilled.txt")
-        print("       --num_mutants: run with specific number of mutants")
+        print("       --numMutants: run with specific number of mutants")
         sys.exit(0)
 
     verbose = "--verbose" in sys.argv
@@ -95,17 +95,17 @@ def main():
         args.remove(timeout)
         timeout = float(timeout)
 
-    num_mutants = -1
+    numMutants = -1
     try:
-        nmpos = args.index("--num_mutants")
+        nmpos = args.index("--numMutants")
     except ValueError:
         nmpos = -1
 
     if nmpos != -1:
-        num_mutants = args[topos + 1]
-        args.remove("--num_mutants")
-        args.remove(num_mutants)
-        num_mutants = int(num_mutants)
+        numMutants = args[topos + 1]
+        args.remove("--numMutants")
+        args.remove(numMutants)
+        numMutants = int(numMutants)
 
     onlyMutants = None
     if fromFile is not None:
@@ -163,7 +163,7 @@ def main():
 
     if not noShuffle:
         random.shuffle(allTheMutants)
-        allTheMutants = allTheMutants[:num_mutants] if num_mutants != -1 else allTheMutants
+        allTheMutants = allTheMutants[:numMutants] if numMutants != -1 else allTheMutants
 
     allStart = time.time()
 
