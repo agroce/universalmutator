2d67a8e8c2a8b7f9f15991574b17528e1eb118ef
Alex Groce <agroce@gmail.com>
2022-05-18 11:21:48 -0700

    New version for pip release later today

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='universalmutator',
-    version='1.0.22',
+    version='1.0.23',
     description='Universal regexp-based mutation tool',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
