8e2b82c7d06b127c5f5d0cac1b4a6f703b0d1058
Ethan Heimer <crooked@Ethans-MacBook-Pro.local>
2026-04-29 22:03:48 -0700

    PyLint Falure Fixes

--- tests/mutantDir_tests.py
+++ tests/mutantDir_tests.py
@@ -16,7 +16,7 @@ def UtilCreateTestingDir():
         os.mkdir("./"+test_directory_name)
 
 # execute 'mutate ../examples/foo.py --mutantDir {test_directory_name}', returns true if no errors thrown, false otherwise
-def ExecuteMutateWithMutantDir(test_directory): 
+def ExecuteMutateWithMutantDir(test_directory):
     try:
         subprocess.run(["mutate", "../examples/foo.py", "--mutantDir", test_directory], check=True)
         return True
@@ -46,22 +46,22 @@ class TestMutantDir(unittest.TestCase):
         self.assertEqual(ExecuteMutateWithMutantDir(test_directory_name), True)
 
     # When the dir passed into mutantDir is multiple layers deep (./Parent/Child), and the parent directory does not exist, the program should exit with an error.
-    def test_mutantDir_FileNotFoundError(self): 
+    def test_mutantDir_FileNotFoundError(self):
         UtilRemoveTestingDir()
-        self.assertEqual(ExecuteMutateWithMutantDir(test_directory_name+"/Test2"), False) 
+        self.assertEqual(ExecuteMutateWithMutantDir(test_directory_name+"/Test2"), False)
 
     # When mutate does not have permissions to write into the file passed into --mutantDir, throw a PermissionError and exit.
-    def test_mutantDir_PermissionError(self): 
+    def test_mutantDir_PermissionError(self):
         UtilRemoveTestingDir()
         # Create Read Only Dir
         os.mkdir(test_directory_name+"ReadOnly")
         os.chmod(test_directory_name+"ReadOnly", stat.S_IRUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH)
 
-        self.assertEqual(ExecuteMutateWithMutantDir(test_directory_name+"ReadOnly/Test"), False) 
+        self.assertEqual(ExecuteMutateWithMutantDir(test_directory_name+"ReadOnly/Test"), False)
 
         # Remove Read Only Dir
         os.rmdir(test_directory_name+"ReadOnly")
-    
+
     # When the dir passed into mutantDir does not exist, and the --legacyMutantDir flag is pressent, the program should crash
     def test_legacyMutantDir_DoesIgnoreCheck(self):
         UtilRemoveTestingDir()
@@ -82,4 +82,3 @@ if __name__ == '__main__':
 
     # Clean Testing Enviorment
     UtilRemoveTestingDir()
-
